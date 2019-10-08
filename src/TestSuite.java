b697ae69e44c5c213f4ae235a5857c1415eb84e1
sune <sune1309@edu.easj.dk>
2019-10-08 11:43:14 +0200

    Initial commit

---
+++ src/TestSuite.java
@@ -0,0 +1,12 @@
+public abstract class TestSuite {
+
+    public void  runTestSuite () {
+        // TODO skal gøre et eller andet.
+    }
+protected abstract void testliste();
+
+    public  void  assertEquals (int expected, int actual) {
+        // TODO
+    }
+
+}
